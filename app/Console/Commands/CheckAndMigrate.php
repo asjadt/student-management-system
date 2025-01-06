a0e274f3da8f21ce28f65465917cf0ecd464f6b6
rifat32 <rifatbilalphilips@gmail.com>
2025-01-06 21:52:39 +0600

    development task

--- app/Console/Commands/CheckAndMigrate.php
+++ app/Console/Commands/CheckAndMigrate.php
@@ -6,7 +6,8 @@ namespace App\Console\Commands;
     use Illuminate\Console\Command;
     use Illuminate\Support\Facades\Schema;
     use Illuminate\Support\Facades\Artisan;
-    use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
     class CheckAndMigrate extends Command
     {
